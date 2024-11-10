959d57464f2b0f3a3a7d1873402a1cb33bfc9eca
prajyot-porje <prajyotyt18@gmail.com>
2024-11-10 22:36:25 +0530

    formatted

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ export default function Home() {
   return (
     <div className=" relative bg-black-100  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className=" max-w-7xl w-full">
-        <FloatingNav
-        navItems={[{name: 'home', link : '/'}]}
-        />
+        <FloatingNav navItems={[{name: 'home', link : '/'}]} />
         <Hero/>
         <ABOUT/>
         <Projects/>
