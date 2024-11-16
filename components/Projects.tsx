79721c66e2ae0424654a0a71571718c284c558e6
prajyot-porje <prajyotyt18@gmail.com>
2024-11-16 23:55:07 +0530

    changing project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,6 @@
 import { projects } from "@/data";
 import React from "react";
-import { PinContainer } from "./ui/3d-pin";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 const Projects = () => {
   return (
@@ -14,42 +14,49 @@ const Projects = () => {
             key={project.id}
             className=" sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
-            <PinContainer title="details" href={project.git}>
-                    <div className=" relative flex items-center justify-center sm:w-[570px] w-[80vw] sm:h-[40vh] h-[30vh] overflow-hidden  mb-10
-                    ">
-                        <div className=" relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                            <img
-                            src={project.img}
-                            alt={project.title}
-                            />
-                        </div>
-                        <img
-                        src={project.img}
-                        alt={project.title}
-                        className=" absolute z-10 bottom-0"
-                        />
-                    </div>
-                    <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                        {project.title}
-                    </h1>
-                    <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-                        {project.des}
-                    </p>
-                    <div className="flex items-center justify-between mt-7 mb-3">
-                        <div className="flex items-center">
-                            {project.iconLists.map((icon , index) =>(
-                                <div key={icon} className=" border border-white/[0.4] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                                style={{transform:`translateX(-${5*index*2}px)`}}
-                                >                                    <img
-                                    src={icon}
-                                    alt={icon}
-                                    className="p-2"
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-            </PinContainer>
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  {project.title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {project.des}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4">
+                  <img
+                    src="/lucifer.png"
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <CardItem
+                    translateZ={20}
+                    href={project.link}
+                    target="__blank"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try now →
+                  </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  >
+                    Sign up
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
           </div>
         ))}
       </div>
